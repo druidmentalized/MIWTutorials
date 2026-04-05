2414a527f3fe942c030965855499d883a979a6bc
druidmentalized <dmitry.barmuta@gmail.com>
2026-04-05 21:06:17 +0200

    feat: tailor custom logistic regression implementation to fit sklearn's classifier class

--- Project2/reglog.py
+++ Project2/reglog.py
@@ -1,46 +1,61 @@
 import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
 
-
-class LogisticRegression:
-    
-    def __init__(self, eta=0.05, max_iterations=1000, random_state=5058, tol=0.1e-4):
-        self.rgen = np.random.default_rng(random_state)
-        self.tol = tol
+class LogisticRegression(BaseEstimator, ClassifierMixin):
+    def __init__(self, eta=0.05, max_iterations=1000, random_state=5058, tol=1e-5):
         self.eta = eta
         self.max_iterations = max_iterations
-        self.prev_cost = np.inf 
-    
-    def fit(self, X, y): 
-        n_samples, _ = X.shape
-        
-        self.weights = self.rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
-        self.bias = np.float64(0.0)
-        
-        for i in range(self.max_iterations):
-            y_predicted = self.predict_proba(X)
-            
-            errors = (y_predicted - y)
-            norm_gradient = 1 / n_samples
-            
-            self.weights -= self.eta * norm_gradient * X.T.dot(errors)
-            self.bias -= self.eta * norm_gradient * np.sum(errors)
+        self.random_state = random_state
+        self.tol = tol
 
-            cost = (-y.dot(np.log(y_predicted)) - ((1 - y).dot(np.log(1 - y_predicted))))
-            if i > 0 and abs(self.prev_cost - cost) < self.tol:
-                break
-                
-            self.prev_cost = cost
+    def __sklearn_tags__(self):
+        tags = super().__sklearn_tags__()
+        tags.estimator_type = "classifier"
+        return tags
+
+    def fit(self, X, y):
+        X, y = check_X_y(X, y)
+        self.classes_ = unique_labels(y)
+
+        n_samples, n_features = X.shape
+        rgen = np.random.default_rng(self.random_state)
+
+        self.weights_ = rgen.normal(loc=0.0, scale=0.01, size=n_features)
+        self.bias_ = 0.0
+        prev_cost = np.inf
+
+        for i in range(self.max_iterations):
+            net_input = np.dot(X, self.weights_) + self.bias_
+            y_predicted = self._sigmoid(net_input)
+
+            errors = (y_predicted - y)
+
+            self.weights_ -= self.eta * (1 / n_samples) * X.T.dot(errors)
+            self.bias_ -= self.eta * (1 / n_samples) * np.sum(errors)
+
+            y_clipped = np.clip(y_predicted, 1e-15, 1 - 1e-15)
+            cost = -np.mean(y * np.log(y_clipped) + (1 - y) * np.log(1 - y_clipped))
+
+            if abs(prev_cost - cost) < self.tol: break
+            prev_cost = cost
 
         return self
-    
-    def net_input(self, X):
-        return np.dot(X, self.weights) + self.bias
 
-    def sigmoid_activation(self, z):
+    @staticmethod
+    def _sigmoid(z):
         return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
 
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, 0)
-    
     def predict_proba(self, X):
-        return self.sigmoid_activation(self.net_input(X))
+        check_is_fitted(self)
+        X = check_array(X)
+        
+        net_input = np.dot(X, self.weights_) + self.bias_
+        proba_1 = self._sigmoid(net_input)
+        
+        return np.column_stack([1 - proba_1, proba_1])
+
+    def predict(self, X):
+        probas = self.predict_proba(X)
+        return np.where(probas[:, 1] >= 0.5, self.classes_[1], self.classes_[0])
