815021462517ca7c13682d92eebf63deaa664d1b
druidmentalized <dmitry.barmuta@gmail.com>
2026-04-20 17:59:25 +0200

    fix: add proper path to data, add proper data returns to fir sklearn format

--- Project3/data_provider.py
+++ Project3/data_provider.py
@@ -2,10 +2,10 @@ import pandas as pd
 import glob
 
 
-def load_data(path="data/data*.txt"):
+def load_data(path="dane/dane*.txt"):
     data = pd.concat(
         [pd.read_csv(file, sep="\\s+", header=None, names=["X", "y"])
          for file in sorted(glob.glob(path))],
         ignore_index=True
     )
-    return data[["X"]], data[["y"]]
+    return data[["X"]].values, data["y"].values
