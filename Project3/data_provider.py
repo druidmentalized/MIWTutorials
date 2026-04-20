de196ef6c256f69cb5121c1161c28757270bb1b4
druidmentalized <dmitry.barmuta@gmail.com>
2026-04-20 18:06:45 +0200

    fix: add normalization to parsed data

--- Project3/data_provider.py
+++ Project3/data_provider.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import glob
 
+from sklearn.preprocessing import MinMaxScaler
+
 
 def load_data(path="dane/dane*.txt"):
     data = pd.concat(
@@ -9,3 +11,16 @@ def load_data(path="dane/dane*.txt"):
         ignore_index=True
     )
     return data[["X"]].values, data["y"].values
+
+def normalize_data(split_data):
+    X_train, X_test, y_train, y_test = split_data
+
+    scaler_X = MinMaxScaler()
+    X_train = scaler_X.fit_transform(X_train)
+    X_test = scaler_X.transform(X_test)
+
+    scaler_y = MinMaxScaler()
+    y_train = scaler_y.fit_transform(y_train.reshape(-1, 1)).ravel()
+    y_test = scaler_y.transform(y_test.reshape(-1, 1)).ravel()
+
+    return X_train, X_test, y_train, y_test
